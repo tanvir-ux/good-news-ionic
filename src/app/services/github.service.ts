17d88e37e6726ea910d5880bce92534ce4eae6d9
tanvir-ux <tanviralamshuvo@gmail.com>
2021-08-26 15:12:34 +0600

    fetch from github raw json success

---
+++ src/app/services/github.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  url: any = 'https://raw.githubusercontent.com/tanvir-ux/GoodNews/develop/news.json'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getGoodNews(): Observable<any> {
+    return this.http.get<any>(this.url)
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../services/github.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  data: Observable<any>
 
+  constructor(
+    private githubService: GithubService
+  ) { 
+
+    
+
+   }
+
+  ionViewDidEnter() {
+    this.githubService.getGoodNews().subscribe((resp: any) => {
+      this.data = resp;
+    })
+  }
+
+  ngOnInit() {
+    
+  }
 }
